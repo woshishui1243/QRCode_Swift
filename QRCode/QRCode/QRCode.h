42f0860cd9ccd0c6702d16c71eb10516d8aae1ed
woshishui1243 <dayu_0518@163.com>
2015-06-30 16:40:37 +0800

    配置git

---
+++ QRCode/QRCode/QRCode.h
@@ -0,0 +1,19 @@
+//
+//  QRCode.h
+//  QRCode
+//
+//  Created by dayu on 15/6/30.
+//  Copyright (c) 2015年 dayu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCode.
+FOUNDATION_EXPORT double QRCodeVersionNumber;
+
+//! Project version string for QRCode.
+FOUNDATION_EXPORT const unsigned char QRCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCode/PublicHeader.h>
+
+
---
+++ QRCodeDemo/QRCodeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCodeDemo
+//
+//  Created by dayu on 15/6/30.
+//  Copyright (c) 2015年 dayu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
